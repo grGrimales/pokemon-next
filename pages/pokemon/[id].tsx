762d793211a361bf70ef08470b3550edc89d28c5
GrGrimales <grojasgrimales@gmail.com>
2022-05-27 10:39:39 -0500

    Se implemento incrementalStatic

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -120,17 +120,28 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
         paths: pokemos151.map(id => ({
             params: { id }
         })),
-        fallback: false
+        fallback: 'blocking'
     }
 
 }
 
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const { id } = params as { id: string }
+    const { id } = params as { id: string };
+    const pokemon = await getPokemonInfo(id);
+
+    if (!pokemon) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
 
     return {
-        props: { pokemon: await getPokemonInfo(id) }
+        props: { pokemon },
+        revalidate: 86400,
     }
 
 }
